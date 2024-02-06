e66d810534f90b0c91556c998f8990c621eb6404
Axel Encalada-Arce <axelarce84@gmail.com>
2024-02-05 23:53:03 -0600

    changed the whole layout of the website and added pictures and buttons

--- script.js
+++ script.js
@@ -4,11 +4,41 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (e) {
       e.preventDefault();
       document.getElementById("mainContent").style.display = "none";
+      document.getElementById("GradButtonContent").style.display = "none";
+      document.getElementById("WedButtonContent").style.display = "none";
+      document.getElementById("MiscButtonContent").style.display = "none";
       document.getElementById("aboutMeContent").style.display = "block";
     });
   document.getElementById("goHomeLink").addEventListener("click", function (e) {
     e.preventDefault();
     document.getElementById("aboutMeContent").style.display = "none";
+    document.getElementById("GradButtonContent").style.display = "none";
+    document.getElementById("WedButtonContent").style.display = "none";
+    document.getElementById("MiscButtonContent").style.display = "none";
     document.getElementById("mainContent").style.display = "block";
+  });
+  document.getElementById("GradButton").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("mainContent").style.display = "none";
+    document.getElementById("aboutMeContent").style.display = "none";
+    document.getElementById("WedButtonContent").style.display = "none";
+    document.getElementById("MiscButtonContent").style.display = "none";
+    document.getElementById("GradButtonContent").style.display = "block";
+  });
+  document.getElementById("WedButton").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("mainContent").style.display = "none";
+    document.getElementById("aboutMeContent").style.display = "none";
+    document.getElementById("MiscButtonContent").style.display = "none";
+    document.getElementById("GradButtonContent").style.display = "none";
+    document.getElementById("WedButtonContent").style.display = "block";
+  });
+  document.getElementById("MiscButton").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("mainContent").style.display = "none";
+    document.getElementById("aboutMeContent").style.display = "none";
+    document.getElementById("WedButtonContent").style.display = "none";
+    document.getElementById("GradButtonContent").style.display = "none";
+    document.getElementById("MiscButtonContent").style.display = "block";
   });
 });
