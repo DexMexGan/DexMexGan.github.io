7dc2d5f551f3e2e85e15ed34964d65162e13e436
Axel Encalada-Arce <axelarce84@gmail.com>
2024-02-27 15:10:31 -0600

    Removed some javascript, css and html sections and made the buttons take you directly to a from

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (e) {
       e.preventDefault();
       document.getElementById("mainContent").style.display = "none";
-      document.getElementById("GradButtonContent").style.display = "none";
-      document.getElementById("WedButtonContent").style.display = "none";
-      document.getElementById("MiscButtonContent").style.display = "none";
       document.getElementById("portfolioSection").style.display = "none";
       document.getElementById("contactSection").style.display = "none";
       document.getElementById("aboutMeContent").style.display = "block";
@@ -14,42 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("goHomeLink").addEventListener("click", function (e) {
     e.preventDefault();
     document.getElementById("aboutMeContent").style.display = "none";
-    document.getElementById("GradButtonContent").style.display = "none";
-    document.getElementById("WedButtonContent").style.display = "none";
-    document.getElementById("MiscButtonContent").style.display = "none";
     document.getElementById("portfolioSection").style.display = "none";
     document.getElementById("contactSection").style.display = "none";
     document.getElementById("mainContent").style.display = "block";
-  });
-  document.getElementById("GradButton").addEventListener("click", function (e) {
-    e.preventDefault();
-    document.getElementById("mainContent").style.display = "none";
-    document.getElementById("aboutMeContent").style.display = "none";
-    document.getElementById("WedButtonContent").style.display = "none";
-    document.getElementById("MiscButtonContent").style.display = "none";
-    document.getElementById("portfolioSection").style.display = "none";
-    document.getElementById("contactSection").style.display = "none";
-    document.getElementById("GradButtonContent").style.display = "block";
-  });
-  document.getElementById("WedButton").addEventListener("click", function (e) {
-    e.preventDefault();
-    document.getElementById("mainContent").style.display = "none";
-    document.getElementById("aboutMeContent").style.display = "none";
-    document.getElementById("MiscButtonContent").style.display = "none";
-    document.getElementById("GradButtonContent").style.display = "none";
-    document.getElementById("portfolioSection").style.display = "none";
-    document.getElementById("contactSection").style.display = "none";
-    document.getElementById("WedButtonContent").style.display = "block";
-  });
-  document.getElementById("MiscButton").addEventListener("click", function (e) {
-    e.preventDefault();
-    document.getElementById("mainContent").style.display = "none";
-    document.getElementById("aboutMeContent").style.display = "none";
-    document.getElementById("WedButtonContent").style.display = "none";
-    document.getElementById("GradButtonContent").style.display = "none";
-    document.getElementById("portfolioSection").style.display = "none";
-    document.getElementById("contactSection").style.display = "none";
-    document.getElementById("MiscButtonContent").style.display = "block";
   });
   document
     .getElementById("portfolioLink")
@@ -57,9 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
       document.getElementById("mainContent").style.display = "none";
       document.getElementById("aboutMeContent").style.display = "none";
-      document.getElementById("WedButtonContent").style.display = "none";
-      document.getElementById("GradButtonContent").style.display = "none";
-      document.getElementById("MiscButtonContent").style.display = "none";
       document.getElementById("contactSection").style.display = "none";
       // This next line should target 'portfolioSection', not 'portfolioContent' or 'contactContent'.
       document.getElementById("portfolioSection").style.display = "block";
@@ -72,10 +33,16 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("mainContent").style.display = "none";
       document.getElementById("aboutMeContent").style.display = "none";
       document.getElementById("portfolioSection").style.display = "none";
-      document.getElementById("GradButtonContent").style.display = "none";
-      document.getElementById("WedButtonContent").style.display = "none";
-      document.getElementById("MiscButtonContent").style.display = "none";
       // Display the contact section
       document.getElementById("contactSection").style.display = "block";
     });
+  document.getElementById("GradButton").addEventListener("click", function () {
+    window.location.href = "https://forms.gle/tErSWdTsZosv5ayX8";
+  });
+  document.getElementById("WedButton").addEventListener("click", function () {
+    window.location.href = "https://forms.gle/ARkPTMjhG1XJFGPfA";
+  });
+  document.getElementById("MiscButton").addEventListener("click", function () {
+    window.location.href = "https://forms.gle/p4fKCYRQe1DGSCg17";
+  });
 });
